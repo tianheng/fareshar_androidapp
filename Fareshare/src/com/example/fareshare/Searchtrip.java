18c3f82e42b67fd1e35ddf0d53e316579be20e2b
tianheng <tianheng.1993@gmail.com>
2013-10-12 03:32:29 +0800

    241

--- Fareshare/src/com/example/fareshare/Searchtrip.java
+++ Fareshare/src/com/example/fareshare/Searchtrip.java
@@ -3,13 +3,16 @@ package com.example.fareshare;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,6 +27,7 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Searchtrip extends Activity {
@@ -40,12 +44,18 @@ public class Searchtrip extends Activity {
 	    String radius = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_RADIUS);
 	    String result = null;
         InputStream is = null;
-        StringBuilder sb=null;
+        StringBuilder NameValuePair;      
+        ArrayList<NameValuePair> postParameters;
+        postParameters = new ArrayList<NameValuePair>();
+        postParameters.add(new BasicNameValuePair("Origin", origin));
+        postParameters.add(new BasicNameValuePair("Destination", destination));
+        postParameters.add(new BasicNameValuePair("Radius",radius));
+
 	    //http post
         try{
             HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost("http://127.0.0.1/food.php");
-           // httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpPost httppost = new HttpPost("http://ec2-54-214-246-100.us-west-2.compute.amazonaws.com/Controllers/TripController.php");
+            httppost.setEntity(new UrlEncodedFormEntity(postParameters));
             HttpResponse response = httpclient.execute(httppost);
             HttpEntity entity = response.getEntity();
             is = entity.getContent();
@@ -56,7 +66,7 @@ public class Searchtrip extends Activity {
         //convert response to string
         try{
             BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-            sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             sb.append(reader.readLine() + "\n");
             String line="0";
           
@@ -72,16 +82,22 @@ public class Searchtrip extends Activity {
         }
  
         //paring data
-        int fd_id;
-        String fd_name;
         try{
-        jArray = new JSONArray(result);
+        JSONArray jArray = new JSONArray(result);
         JSONObject json_data=null;
-         
+        String PosterID;
+        String TripID;
+        String DriverID;
+        String fd_name;
         for(int i=0;i<jArray.length();i++){
                 json_data = jArray.getJSONObject(i);
-                fd_id=json_data.getInt("FOOD_ID");
-                fd_name=json_data.getString("FOOD_NAME");
+                PosterID=json_data.getString("Success");
+                TripID=json_data.getString("TripID");
+                DriverID=json_data.getString("DriverID");
+                TextView textView = new TextView(this);
+        	    textView.setTextSize(40);
+        	    textView.setText(PosterID);
+                setContentView(textView);
         }
          
         }catch(JSONException e1){
